01b331d54099c3b401fe6d5f2d0290181a133324
Oliveira <sma.samuel.17@gmail.com>
2026-09-14 20:47:51 -0500

    fix: usar 4 espacos ao inves de tab de 8 colunas em TODOS os arquivos

--- init.lua
+++ init.lua
@@ -1047,7 +1047,7 @@ end
 --
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'php',
+  --pattern = 'php',
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
